963bde9323f36d726908604ab9171424cb474e41
Jinny <jinny@Jinnyui-MacBookPro.local>
2026-01-15 01:15:15 +0900

    todolist commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import { useState } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
+
 const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: 'react base',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: 'react styling',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: 'schdual management',
+      checked: false,
+    },
+  ]);
+
   return (
     <TodoTemplate>
       <TodoInsert />
+      <TodoList todos={todos} />
     </TodoTemplate>
   );
 };
